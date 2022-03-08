6de58a04f3c5c6d1230ac7d9dad1c81ae7d00f0c
Alexey Bogoslovsky <lekhabog@gmail.com>
2022-03-09 01:19:43 +0300

    final version 7.2

--- python4everybody/p4e-a07.2.py
+++ python4everybody/p4e-a07.2.py
@@ -1,8 +1,13 @@
 # Use the file name mbox-short.txt as the file name
 fname = input("Enter file name: ")
 fh = open(fname)
+count = 0
+total = 0
 for line in fh:
     if not line.startswith("X-DSPAM-Confidence:"):
         continue
-    print(line)
-print("Done")
+    #print(line)
+    total = total + float(line[line.find(':') + 1:].rstrip())
+    count = count + 1    
+print('Average spam confidence:', total/count)
+#print("Done")
