fc025b1ed13ee534db4f8c2feaa4ced587b7a4fb
Alexey Bogoslovsky <lekhabog@gmail.com>
2022-03-09 02:53:54 +0300

    8.4 completed

--- python4everybody/p4e-a08.4.py
+++ python4everybody/p4e-a08.4.py
@@ -1,5 +1,14 @@
 fname = input("Enter file name: ")
 fh = open(fname)
 lst = list()
-for line in fh:
-print(line.rstrip())
+for ln in fh:
+    line = ln.rstrip()
+    lnlst = line.split()    
+    for miniitem in lnlst:        
+        if miniitem in lst:continue
+        lst.append(miniitem)
+lst.sort()
+print(lst)
+
+
+    
