7ab6b302985affc4d2da6762a871b9997eb5aeec
Alexey Bogoslovsky <lekhabog@gmail.com>
2022-03-11 00:49:07 +0300

    9.4 - done

--- python4everybody/p4e-a09.4.py
+++ python4everybody/p4e-a09.4.py
@@ -1,4 +1,19 @@
 name = input("Enter file:")
 if len(name) < 1:
     name = "mbox-short.txt"
-handle = open(name)
+fn = open(name)
+efreq = dict()
+for ln in fn:
+    line = ln.strip()
+    if not 'From ' in line:continue
+    lst = line.split()
+    email = lst[1]
+    efreq[lst[1]] = efreq.get(lst[1],0) + 1
+popularity = None
+person = None
+for k, v in efreq.items():
+    if popularity is None or v > popularity:
+        popularity = v
+        person = k
+print(person, popularity)
+#print(efreq)
