7ee37300c8b1afc73fd92e03e28b0b1e47105891
Alexey Bogoslovsky <lekhabog@gmail.com>
2022-03-11 16:54:53 +0300

    10.2 completed

--- python4everybody/p4e-a10.2.py
+++ python4everybody/p4e-a10.2.py
@@ -1,2 +1,26 @@
-x,y=3,4
-print(x,y)
+name = input("Enter file:")
+if len(name) < 1:
+    name = "mbox-short.txt"
+fn = open(name)
+hours = dict()
+
+for ln in fn:
+    line = ln.strip()
+    if not 'From ' in line:continue
+    lst = line.split()
+    fulltime = lst[5]
+    sfulltime = fulltime.split(':')
+    hourvalue = sfulltime[0]
+    hours[hourvalue] = hours.get(hourvalue, 0) + 1
+thours = sorted(hours.items())
+for k,v in thours:
+    print(k,v)
+
+#popularity = None
+#person = None
+#for k, v in efreq.items():
+    #if popularity is None or v > popularity:
+     #   popularity = v
+      #  person = k
+#print(person, popularity)
+#print(efreq)
