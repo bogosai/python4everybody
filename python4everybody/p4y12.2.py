47c328991127028395dcc79d09a02c341436a3d5
Alexey Bogoslovsky <lekhabog@outlook.com>
2022-03-29 16:38:15 +0300

    completed 12

--- python4everybody/p4y12.2.py
+++ python4everybody/p4y12.2.py
@@ -1,8 +1,5 @@
-# To run this, download the BeautifulSoup zip file
-# http://www.py4e.com/code3/bs4.zip
-# and unzip it in the same directory as this file
 
-import urllib.request, urllib.parse, urllib.error #from urllib.request  import urlopen
+import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import ssl
 
@@ -11,19 +8,27 @@ ctx = ssl.create_default_context()
 ctx.check_hostname = False
 ctx.verify_mode = ssl.CERT_NONE
 
-url = input('Enter - ')
+url = input('Enter link - ')
+ipos = input('Position - ')
+nbD = input('Number of drills - ')
+nbDrills = int(nbD)
+iposition = int(ipos)
+
 html = urllib.request.urlopen(url, context=ctx).read()
 soup = BeautifulSoup(html, "html.parser")
 
-# Retrieve all of the anchor tags
-
-tags = soup('span')
-sumSpan  = 0
-for tag in tags:
-    # Look at the parts of a tag
-    #print('TAG:', tag)
-    #print('URL:', tag.get('href', None))
-    #print('Contents:', tag.contents[0])
-    sumSpan = sumSpan + int(tag.contents[0])
-    #print('Attrs:', tag.attrs)
-print(sumSpan)
+tags = soup('a')
+print(url)
+index = 1
+position = 1
+while index <= nbDrills:
+    for tag in tags:        
+        if position == iposition:                     
+            print(tag.contents[0])
+            url = tag.get('href', None)            
+            html = urllib.request.urlopen(url, context=ctx).read()            
+            soup = BeautifulSoup(html, "html.parser")            
+            tags = soup('a')            
+        position = position + 1
+    position = 1
+    index = index + 1
